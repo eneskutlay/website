43027e8a4dada3dbad21952253881fa4fbf69849
Enes Kutlay <contact@eneskutlay.dev>
2021-09-07 03:03:09 +0300

    loader function added

--- components/Projects.js
+++ components/Projects.js
@@ -2,10 +2,13 @@ import styles from "../styles/Home.module.css";
 import Image from "next/image";
 
 const Projects = (props) => {
+  const myLoader = ({ src, width, quality }) => {
+    return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+  }
   return (
     <div className={styles.projectscard}>
       <h3>{props.title}</h3>
-      <Image src={props.src} alt={props.alt} width={250} height={160} />
+      <Image loader={myLoader} src={props.src} alt={props.alt} width={250} height={160}  />
       <p>
         {props.details}
         <span>
