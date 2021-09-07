446954b30f2140be09cfba85dfeca607c97f3f23
Enes Kutlay <contact@eneskutlay.dev>
2021-09-07 03:02:31 +0300

    Added nofollow tag

--- components/Social.js
+++ components/Social.js
@@ -4,20 +4,20 @@ import Card from "./Card";
 const Social = () => {
   return (
     <div className={styles.social}>
-      <a href="https://github.com/eneskutlay" rel="noopener" target="_blank">
+      <a href="https://github.com/eneskutlay" rel="nofollow" target="_blank">
         <Card src="/github.svg" alt="Github Icon" title="Github Profile" />
       </a>
-      <a href="https://twitter.com/enesevski" rel="noopener" target="_blank">
+      <a href="https://twitter.com/enesevski" rel="nofollow" target="_blank">
         <Card src="/twitter.svg" alt="Twitter Icon" title="Twitter Profile" />
       </a>
-      <a href="https://www.linkedin.com/in/eneskutlay/" rel="noopener" target="_blank">
+      <a href="https://www.linkedin.com/in/eneskutlay/" rel="nofollow" target="_blank">
         <Card
           src="/linkedin.svg"
           alt="Linkedin Icon"
           title="Linkedin Profile"
         />
       </a>
-      <a href="https://blog.eneskutlay.dev/" rel="noopener" target="_blank">
+      <a href="https://blog.eneskutlay.dev/" rel="nofollow" target="_blank">
         <Card src="/hashnode.svg" alt="Hashnode Icon" title="Blog Posts" />
       </a>
     </div>
