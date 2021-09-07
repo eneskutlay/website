f58bddd95fe2eb94bb05ea45f7f850f301a40a09
Enes Kutlay <contact@eneskutlay.dev>
2021-09-07 03:02:01 +0300

    SEO bugs fixed

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,19 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Enes Kutlay</title>
+        <title>Enes Kutlay - Personal Website</title>
         <meta name="theme-color" content="#FFFFFF" />
-        <meta name="description" content="Enes Kutlay personal website" />
+        <meta name="description" content="I am interested in web development and specifically front-end development." />
         <meta name="author" content="Enes Kutlay" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="Personal website and portolio" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="/og.png" />
-        <meta property="og:url" content="https://eneskutlay.dev/" />
+        <meta name="robots" content="all" />
+        <meta name="publisher" content="Enes Kutlay" />
+        <meta property = "og: type" content = "website" />
+        <meta property = "og: title" content = " Enes Kutlay - Personal Website" />
+        <meta property = "og: description" content = "Hi 👋 I am Enes. I am interested in web development and specifically front-end development." />
+        <meta property = "og: image" content = "/og.png" />
+        <meta property = "og: url" content = "https://eneskutlay.dev/" />
+        <meta property = "og: site_name" content = "Enes Kutlay" />
         <link rel="icon" href="/me.ico" />
       </Head>
       <Navbar />
