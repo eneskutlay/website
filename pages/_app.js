0708662ff9eb92560c83dee59d7ed1da288ad0ef
Enes Kutlay <contact@eneskutlay.dev>
2021-09-16 18:06:19 +0300

    og tag not working

--- pages/_app.js
+++ pages/_app.js
@@ -14,12 +14,12 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="all" />
         <meta name="publisher" content="Enes Kutlay" />
-        <meta property = "og: type" content = "website" />
-        <meta property = "og: title" content = " Enes Kutlay - Personal Website" />
-        <meta property = "og: description" content = "Hi 👋 I am Enes. I am interested in web development and specifically front-end development." />
-        <meta property = "og: image" content = "/og.jpeg" />
-        <meta property = "og: url" content = "https://eneskutlay.dev/" />
-        <meta property = "og: site_name" content = "Enes Kutlay" />
+        <meta property = "og:type" content = "website" />
+        <meta property = "og:title" content = " Enes Kutlay - Personal Website" />
+        <meta property = "og:description" content = "Hi 👋 I am Enes. I am interested in web development and specifically front-end development." />
+        <meta property = "og:image" content = "/og.jpeg" />
+        <meta property = "og:url" content = "https://eneskutlay.dev/" />
+        <meta property = "og:site_name" content = "Enes Kutlay" />
         <link rel="icon" href="/me.ico" />
       </Head>
       <Navbar />
