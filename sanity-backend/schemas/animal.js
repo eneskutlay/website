e2ce5ccc45892daba3e4c88bdc1a67ba4f331416
Enes Kutlay <contact@eneskutlay.dev>
2022-08-29 03:54:33 +0000

    sanity backend added

---
+++ sanity-backend/schemas/animal.js
@@ -0,0 +1,17 @@
+export default {
+  name: "animal",
+  type: "document",
+  title: "Animal",
+  fields: [
+    {
+      name: "type",
+      type: "string",
+      title: "Tür",
+    },
+    {
+      name: "name",
+      type: "string",
+      title: "İsim",
+    },
+  ],
+};
