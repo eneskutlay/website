7a0b36cd2ba1680db1642add201ccff4b8cea0a5
Enes Kutlay <contact@eneskutlay.dev>
2022-09-30 01:36:29 +0300

    components folder structure completed

--- components/index.js
+++ components/index.js
@@ -1,8 +1,8 @@
-import Header from "./Header/Header";
-import Hero from "./Hero/Hero";
-import Homesection from "./HomeSection/HomeSection";
-import PostItem from "./PostItem/PostItem";
-import WriteSection from "./WriteSection/WriteSection";
-import ListItem from "./ListItem/ListItem";
+import Header from "./Header";
+import Hero from "./Hero";
+import Homesection from "./HomeSection";
+import PostItem from "./PostItem";
+import WriteSection from "./WriteSection";
+import ListItem from "./ListItem";
 
 export {Header, Hero, Homesection, PostItem, WriteSection, ListItem};
