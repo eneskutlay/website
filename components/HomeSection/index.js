5d23c043ec4605b23d9b73e5a02cb9ef082d140f
Enes Kutlay <contact@eneskutlay.dev>
2022-11-17 17:59:38 +0300

    Post check added

--- components/HomeSection/index.js
+++ components/HomeSection/index.js
@@ -6,12 +6,20 @@ export default function HomeSection({ children, topic, viewUrl }) {
       <div className={styles.container}>
         <div className={styles.cardHeader}>
           <h2>{topic}</h2>
-          <a target="_blank" rel="noopener noreferrer" href={viewUrl}>
-            View All
-          </a>
+          <a href={viewUrl}>View All</a>
         </div>
       </div>
-      <div className={styles.realChild}>{children}</div>
+      {(() => {
+        if (children.length > 0) {
+          return <div className={styles.realChild}>{children}</div>;
+        } else {
+          return (
+            <div className={styles.realChild}>
+              <h3>No blog posts have been published yet.</h3>
+            </div>
+          );
+        }
+      })()}
     </div>
   );
 }
--- components/ListItem/index.js
+++ components/ListItem/index.js
@@ -1,12 +1,11 @@
 import styles from "./ListItem.module.css";
-
 export default function ListItem(props) {
   return (
     <a
+      target={props.target}
       className={styles.cardChild}
       rel="noopener noreferrer"
       href={props.url}
-      target="_blank"
     >
       {props.title}
       <p>{props.detail}</p>
