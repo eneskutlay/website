3c257685650a1ae97ae09c2a2adc9e0eccdcf305
Enes Kutlay <contact@eneskutlay.dev>
2022-11-17 17:58:55 +0300

    Blog section update

--- pages/writing/[slug].js
+++ pages/writing/[slug].js
@@ -3,19 +3,34 @@ import styles from "../../styles/Post.module.css";
 import { PortableText } from "@portabletext/react";
 import { componentsConf } from "../../lib/portableComponent";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Post({ post }) {
   const { body = [] } = post;
   return (
-    <div className={styles.root}>
-      <article className={styles.article}>
-        <h1>{post.title}</h1>
-        <PortableText value={body} components={componentsConf} />
-      </article>
-      <Link href="/writing">
-        <a className={styles.back}>← Go back</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.title} />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content={post.title} key="title" />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app//${encodeURIComponent(
+            post.title
+          )}.?theme=dark&md=0&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg`}
+        />
+      </Head>
+      <div className={styles.root}>
+        <article className={styles.article}>
+          <h1>{post.title}</h1>
+          <PortableText value={body} components={componentsConf} />
+        </article>
+        <Link href="/writing">
+          <a className={styles.back}>← Go back</a>
+        </Link>
+      </div>
+    </>
   );
 }
 
